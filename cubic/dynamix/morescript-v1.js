7032de162a2d7fec7c6759cafd6b133d2b685b29
RenPT23R <renpt.unity@gmail.com>
2025-10-16 14:25:15 +0100

    Update morescript-v1.js

--- cubic/dynamix/morescript-v1.js
+++ cubic/dynamix/morescript-v1.js
@@ -1,4 +1,21 @@
 // morescript.js
+/*
+ * dynamix.js
+ * Copyright (C) 2025 RenPT23
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 const iammorescriptmain = true
 
@@ -96,3 +113,4 @@ const Interface = Object.freeze(MS.interface);
 const Type = Object.freeze(MS.type);
 const Immutable = Object.freeze(MS.immutable);
 const Func = Object.freeze(MS.func);
+
