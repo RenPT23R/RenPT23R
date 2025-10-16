fcbb5f1c0284f3c99a424c13ee6e21767fc8464e
RenPT23R <renpt.unity@gmail.com>
2025-10-16 14:24:46 +0100

    Update dynamix-v1.js

--- cubic/dynamix/dynamix-v1.js
+++ cubic/dynamix/dynamix-v1.js
@@ -1,4 +1,21 @@
 // dynamix.js
+/*
+ * dynamix.js
+ * Copyright (C) 2025 RenPT23
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 // MoreScript!
 
 import "./morescript.js";
@@ -215,3 +232,4 @@ const dynamix = Immutable({
     }
 
 });
+
